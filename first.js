72f5c0858c9375637bf2aa9faecc202b61a72e9f
Ekhara Mustak Pathan <pathanikhara@gmail.com>
2025-12-19 22:11:01 +0530

    Add files via upload

---
+++ first.js
@@ -0,0 +1,12 @@
+console.log("Hello world");
+name = "Tony Stark";
+console.log(name);
+
+console.log(age);
+isFollow = true;
+console.log(isFollow);
+let fullName="Tony";
+console.log(fullName);
+let age=20;
+
+console.log(age);
